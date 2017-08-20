d1c1cf7929bb065dd386c2525ecd1bc0da8e8487
Ola Karlsson <ola.karlsson@bahnhof.se>
2017-08-21 01:39:19 +0200

    First draft

---
+++ src/com/java/web/OffersController.java
@@ -0,0 +1,50 @@
+package com.java.web;
+
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.java.web.dao.Offer;
+import com.java.web.dao.service.OffersService;
+
+@Controller
+public class OffersController {
+	
+	private OffersService offersService;
+	
+	
+	
+	/*
+	@RequestMapping("/")
+	public ModelAndView showHome() {
+		ModelAndView mv = new ModelAndView("home");
+		
+		Map<String, Object> model = mv.getModel();
+		
+		model.put("name", "River");
+		
+		return mv;
+	}*/
+	
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
+	}
+
+	@RequestMapping("/")
+	public String showHome(Model model) {
+		
+		List<Offer> offers = offersService.getCurrent();
+		model.addAttribute("offers", offers);
+		return "home";
+	}
+}
+
